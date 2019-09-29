a8bc44d9cda919dbeff0583efa9a6a3a0dbcf836
ahmedengu <ahmedengu@gmail.com>
2019-09-29 17:58:55 +0200

    required fields check

--- cloud/main.js
+++ cloud/main.js
@@ -1,9 +1,19 @@
+const _ = require('lodash');
 const disposableEmail = require('rendrr-disposable-email-list');
 
 const padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
-
+const requiredFields = ['username', 'email', 'country', 'lang'];
 Parse.Cloud.beforeSave(Parse.User, async (request) => {
   if (request.object.isNew()) {
+    const missing = [];
+    for (const field of requiredFields) {
+      if (!request.object.get(field)) {
+        missing.push(field);
+      }
+    }
+    if (!_.isEmpty(missing)) {
+      throw `${missing.join(', ')} required`;
+    }
     const email = request.object.get('email');
     const emailCheck = disposableEmail.isDisposable(email);
 
@@ -12,8 +22,11 @@ Parse.Cloud.beforeSave(Parse.User, async (request) => {
     }
 
     const query = new Parse.Query(Parse.User);
-    const prefix = email.replace('@', '').replace('.', '').slice(0, 5);
-    const count = await query.startsWith('ref', prefix).count({ useMasterKey: true });
+    const prefix = email.replace('@', '')
+      .replace('.', '')
+      .slice(0, 5);
+    const count = await query.startsWith('ref', prefix)
+      .count({ useMasterKey: true });
 
     request.object.set('ref', `${prefix}${padToThree(count + 1)}`);
     request.object.set('referrals', 0);
