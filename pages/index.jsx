327c19843bd404a41da21e4899639a81d9241112
ahmedengu <ahmedengu@gmail.com>
2019-10-05 22:53:38 +0200

    pass ref on root

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Router from 'next/router';
 
 export default class extends React.Component {
-  static async getInitialProps({ res }) {
+  static async getInitialProps({ res, req }) {
+    const { query: { ref } } = req;
+    const refQS = ref ? `?ref=${ref}` : '';
     if (res) {
       res.writeHead(302, {
-        Location: '/egypt',
+        Location: `/egypt${refQS}`,
       });
       res.end();
     } else {
-      Router.push('/egypt');
+      Router.push(`/egypt${refQS}`);
     }
     return {};
   }
