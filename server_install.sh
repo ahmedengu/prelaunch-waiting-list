d757bca7139fef4b0301b2a418941bfef55b5613
ahmedengu <ahmedengu@gmail.com>
2019-10-10 13:58:33 +0200

    env, youtube

--- server_install.sh
+++ server_install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
 sudo add-apt-repository ppa:ondrej/php
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update -y
