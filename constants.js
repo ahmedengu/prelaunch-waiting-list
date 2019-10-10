5bbb0d7b12c81e3759c3e8088e023e0b89fbb3ef
ahmedengu <ahmedengu@gmail.com>
2019-10-10 15:02:42 +0200

    commit config

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 module.exports = {
   countries: ['egypt', 'saudi', 'emirates'],
   codes: ['EG', 'SA', 'AE'],
-  domain: process.env.PUBLIC_URL || 'http://localhost:3000',
-  serverURL: process.env.SERVER_URL || 'http://localhost:4000/api',
-  applicationId: process.env.APP_ID || 'xxxxx',
-  javaScriptKey: process.env.JAVASCRIPT_KEY || 'xxxxx',
+  domain: 'https://merquant.com',
+  serverURL: 'https://merquant.com/api',
+  applicationId: 'MerQuant',
+  javaScriptKey: 'js',
 };
