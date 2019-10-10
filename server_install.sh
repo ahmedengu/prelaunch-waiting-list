5ee3d3c1add66f3fc6b33fca9b30b59263cf1e5e
ahmedengu <ahmedengu@gmail.com>
2019-10-10 02:16:29 +0200

    server install

---
+++ server_install.sh
@@ -0,0 +1,60 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo add-apt-repository ppa:ondrej/php
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update -y
+
+wget -O- https://raw.github.com/ajenti/ajenti/1.x/scripts/install-ubuntu.sh | sudo sh
+ufw allow 8000
+
+sudo apt-get autoremove && sudo apt-get remove apache2*
+sudo apt-get install -y ajenti-v ajenti-v-nginx ajenti-v-mail ajenti-v-ftp-pureftpd ajenti-v-nodejs ajenti-v-php7.0-fpm php7.0-mysql php7.0-mbstring php7.0-cli php7.0-curl php7.0-dom apache2-utils
+sudo apt-get install -y bind9
+sudo apt-get install -y fail2ban
+
+sudo apt-get install -y mongodb-org python-pip build-essential python-dev libssl-dev unzip --allow-unauthenticated
+sudo pip install pymongo
+sudo systemctl enable mongod
+
+sudo apt-get install -y mysql-server
+sudo mysql_secure_installation
+
+sudo apt-get install -y nodejs
+
+ssh-keygen -t rsa
+
+sudo apt-get install -y gitolite3
+#/root/.ssh/id_rsa.pub
+
+service ajenti restart
+
+
+sudo fallocate -l 4G /swapfile
+sudo mkswap /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+
+#
+#
+#db.createUser(
+#  {
+#    user: "admin",
+#    pwd: "PASSWORD",
+#    roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+#  }
+#)
+#
+#use MerQuantDB
+#
+#db.createUser(
+#    {
+#      user: "MerQuantUser",
+#      pwd: "PASSWORD",
+#      roles: [
+#         { role: "readWrite", db: "MerQuantDB" },
+#      ]
+#    }
+#)
+#
