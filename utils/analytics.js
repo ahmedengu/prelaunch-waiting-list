63e5246d3c152c23dc7ed537c7b1738bfda637e5
ahmedengu <ahmedengu@gmail.com>
2019-10-17 12:59:13 +0200

    facebook pixel

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,11 +1,14 @@
 import ReactGA from 'react-ga';
+import ReactPixel from 'react-facebook-pixel';
 
 export const initGA = () => {
   ReactGA.initialize('UA-149722393-1');
+  ReactPixel.init('2280759418716987');
 };
 export const logPageView = () => {
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
+  ReactPixel.pageView();
 };
 export const logUserId = (id) => {
   ReactGA.set({ userId: id });
@@ -13,10 +16,12 @@ export const logUserId = (id) => {
 export const logEvent = (category = '', action = '') => {
   if (category && action) {
     ReactGA.event({ category, action });
+    ReactPixel.trackCustom(category, action);
   }
 };
 export const logException = (description = '', fatal = false) => {
   if (description) {
     ReactGA.exception({ description, fatal });
+    ReactPixel.trackCustom(description, fatal);
   }
 };
