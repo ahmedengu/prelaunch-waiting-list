f7de50c6cb8c244f7fb2a3bdb516aad26c347426
ahmedengu <ahmedengu@gmail.com>
2019-10-21 00:27:08 +0200

    deploy sh update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd versions
 newDir=$(date +%Y%m%d%H%M%S)
 git clone --single-branch --branch prod git@github.com:ahmedengu/merquant_prelunch.git "$newDir"
 cd "$newDir"
-chown -R www-data:www-data .
+chown -R www-data:www-data "$(pwd)"
 npm i
 #supervisorctl stop all
 npm run build
