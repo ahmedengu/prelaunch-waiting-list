047d45c371818c91a7d3935e17fc85284f5ba83e
ahmedengu <ahmedengu@gmail.com>
2019-10-21 19:53:05 +0200

    remove constants and create prod and local with deploy update

---
+++ constants.local.js
@@ -0,0 +1,8 @@
+module.exports = {
+  countries: ['egypt', 'saudi', 'emirates'],
+  codes: ['EG', 'SA', 'AE'],
+  domain: 'http://localhost:3000',
+  serverURL: 'http://localhost:4000/api',
+  applicationId: 'xxxxx',
+  javaScriptKey: 'xxxxx',
+};
--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,12 @@ newDir=$(date +%Y%m%d%H%M%S)
 git clone --single-branch --branch prod git@github.com:ahmedengu/merquant_prelunch.git "$newDir"
 cd "$newDir"
 chown -R www-data:www-data "$(pwd)"
+cp -rf constants.prod.js constants.js
+
 npm i
 #supervisorctl stop all
 npm run build
+
 ln -sfn "$(pwd)" /srv/deploy/current
 supervisorctl restart all
 
