b08accb0eb024f877f010f25f74a087fed1431ef
ahmedengu <ahmedengu@gmail.com>
2019-10-21 22:03:30 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ cd /srv/deploy/versions
 newDir=$(date +%Y%m%d%H%M%S)
 git clone --single-branch --branch prod git@github.com:ahmedengu/merquant_prelunch.git "$newDir"
 cd "$newDir"
-chown -R www-data:www-data "$(pwd)"
+chown -R www-data:www-data /srv/deploy/
 cp -rf constants.prod.js constants.js
 
 npm i
 #supervisorctl stop all
 npm run build
 
-ln -sfn "$(pwd)" /srv/deploy/current
+ln -sfn /srv/deploy/versions/"$newDir" /srv/deploy/current
 supervisorctl restart all
 
 cp -rf deploy.sh /srv/deploy/deploy.sh
