af973f63fe730b9e20596a7801cc80cf58b5799d
ahmedengu <ahmedengu@gmail.com>
2019-10-22 09:34:09 +0200

    fix analytics bug

--- utils/analytics.js
+++ utils/analytics.js
@@ -2,34 +2,32 @@ import ReactGA from 'react-ga';
 import ReactPixel from 'react-facebook-pixel';
 import { hotjar } from 'react-hotjar';
 
-let initialized = false;
 export const initGA = () => {
-  initialized = true;
   ReactGA.initialize('UA-149722393-1');
   ReactPixel.init('2280759418716987');
   hotjar.initialize(1535519, 6);
 };
 export const logPageView = () => {
-  if (initialized) {
+  if (window.GA_INITIALIZED) {
     ReactGA.set({ page: window.location.pathname });
     ReactGA.pageview(window.location.pathname);
     ReactPixel.pageView();
   }
 };
 export const logUserId = (id) => {
-  if (initialized) {
+  if (window.GA_INITIALIZED) {
     ReactGA.set({ userId: id });
-    ReactGA.fbq('init', '2280759418716987', { uid: id });
+    ReactPixel.fbq('init', '2280759418716987', { uid: id });
   }
 };
 export const logEvent = (category = '', action = '') => {
-  if (category && action && initialized) {
+  if (category && action && window.GA_INITIALIZED) {
     ReactGA.event({ category, action });
     ReactPixel.trackCustom(category, action);
   }
 };
 export const logException = (description = '', fatal = false) => {
-  if (description && initialized) {
+  if (description && window.GA_INITIALIZED) {
     ReactGA.exception({ description, fatal });
     ReactPixel.trackCustom(description, fatal);
   }
