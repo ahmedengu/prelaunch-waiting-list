04ee33ebe5cafa1282413e937d3f8549ad9e8fe9
ahmedengu <ahmedengu@gmail.com>
2019-10-26 00:27:08 +0200

    sent to toast

--- components/unverified.jsx
+++ components/unverified.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Parse from 'parse';
+import { toast } from 'react-toastify';
 import { logEvent } from '../utils/analytics';
 
 class Unverified extends Component {
@@ -14,11 +15,14 @@ class Unverified extends Component {
   }
 
   resend() {
-    const { email } = this.props;
+    const { email, t } = this.props;
     this.setState({ loading: true });
 
     Parse.Cloud.run('resendVerification', { email }).then((reponse) => {
       this.setState({ message: reponse });
+      if (reponse === 'email-sent') {
+        toast(t('sent_to', { email }));
+      }
     }).catch((reason) => {
       this.setState({ message: reason.message });
       logEvent('resendVerification', reason.message);
@@ -45,7 +49,7 @@ class Unverified extends Component {
                     logEvent('user', 'mail-resend');
                   }}
                 >
-                  { t(message)}
+                  {t(message)}
                 </button>
               </h4>
             </div>
