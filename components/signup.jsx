f69ca4676bd518f8bdc2ba971260216d3144cfc4
ahmedengu <ahmedengu@gmail.com>
2020-03-17 03:43:33 +0200

    move backtotop out to smooth scroll

--- components/signup.jsx
+++ components/signup.jsx
@@ -56,7 +56,7 @@ class Signup extends React.Component {
     setPlay(false);
     setTimeout(() => {
       setPlay(true);
-    }, 0);
+    }, 100);
   };
 
   onScroll = () => {
@@ -309,197 +309,203 @@ class Signup extends React.Component {
       );
     });
     return (
-      <div className="scroll-to-top">
-        {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
+      <>
         {this.backToTopButton()}
-        <header id="page-top" className="ts-full-screen">
-          <Header t={t} />
-          <div className="container align-self-center main-content">
-            <div className="row align-items-center">
-              <div className="col-lg-7 col-sm-12 offset-sm-1 signup">
-                <h1 className="typography-head text-center">{ t('Investment-Simplified') }</h1>
-                <h2 className="typography-subhead text-center mb-3">
-                  { t('sub-1') }
-                </h2>
-                <form
-                  className="ts-form ts-form-email ts-labels-inside-input"
-                  noValidate
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    this.register();
-                  }}
-                >
-                  <div className="row text-center align-items-baseline">
-                    <div className="col-2">
-                      <div className="dropdown m-0">
-                        <button
-                          className="btn btn-primary dropdown-toggle"
-                          type="button"
-                          data-toggle="dropdown"
-                        >
-                          <img className="flag" src={`/static/assets/img/${country}.png`} alt="" />
-                          <span className="caret" />
-                        </button>
-                        <ul className="dropdown-menu">
-                          <CountriesSelector />
-                        </ul>
+
+        <div className="scroll-to-top">
+          <header id="page-top" className="ts-full-screen">
+            <Header t={t} />
+            <div className="container align-self-center main-content">
+              <div className="row align-items-center">
+                <div className="col-lg-7 col-sm-12 offset-sm-1 signup">
+                  <h1 className="typography-head text-center">{t('Investment-Simplified')}</h1>
+                  <h2 className="typography-subhead text-center mb-3">
+                    {t('sub-1')}
+                  </h2>
+                  <form
+                    className="ts-form ts-form-email ts-labels-inside-input"
+                    noValidate
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      this.register();
+                    }}
+                  >
+                    <div className="row text-center align-items-baseline">
+                      <div className="col-2">
+                        <div className="dropdown m-0">
+                          <button
+                            className="btn btn-primary dropdown-toggle"
+                            type="button"
+                            data-toggle="dropdown"
+                          >
+                            <img className="flag" src={`/static/assets/img/${country}.png`} alt="" />
+                            <span className="caret" />
+                          </button>
+                          <ul className="dropdown-menu">
+                            <CountriesSelector />
+                          </ul>
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-7 p-0">
-                      <div className="form-group mb-0">
-                        <input
-                          style={{ direction: 'ltr', textAlign: 'left' }}
-                          dir="ltr"
-                          type="search"
-                          className={`signup-input form-control ${error ? 'is-invalid' : ''}`}
-                          placeholder={t('enter_email')}
-                          value={email}
-                          onChange={(event) => {
-                            this.setState({
-                              email: event.target.value,
-                              error: '',
-                            });
-                          }}
-                          id="email-subscribe"
-                          aria-describedby="subscribe"
-                          name="email"
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className="col-3 p-0">
-                      <button
-                        className={`btn btn-primary submit-a ${loading && 'processing'}`}
-                        type="submit"
-                        disabled={loading}
-                      >
-                        {t('submit')}
-                        <div className="status">
-                          <i
-                            className="fas fa-circle-notch fa-spin spinner"
+                      <div className="col-7 p-0">
+                        <div className="form-group mb-0">
+                          <input
+                            style={{ direction: 'ltr', textAlign: 'left' }}
+                            dir="ltr"
+                            type="search"
+                            className={`signup-input form-control ${error ? 'is-invalid' : ''}`}
+                            placeholder={t('enter_email')}
+                            value={email}
+                            onChange={(event) => {
+                              this.setState({
+                                email: event.target.value,
+                                error: '',
+                              });
+                            }}
+                            id="email-subscribe"
+                            aria-describedby="subscribe"
+                            name="email"
+                            required
                           />
                         </div>
+                      </div>
+                      <div className="col-3 p-0">
+                        <button
+                          className={`btn btn-primary submit-a ${loading && 'processing'}`}
+                          type="submit"
+                          disabled={loading}
+                        >
+                          {t('submit')}
+                          <div className="status">
+                            <i
+                              className="fas fa-circle-notch fa-spin spinner"
+                            />
+                          </div>
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                  {error && (<p className="text-danger" style={{ margin: 0 }}>{t(error)}</p>)}
+                  <div className="row">
+                    <div className="col-6 p-0">
+                      <FacebookLogin
+                        isMobile={false}
+                        appId="403863870540210"
+                        fields="email"
+                        callback={(res) => {
+                          this.responseFacebook(res);
+                        }}
+                        render={(renderProps) => (
+                          <FacebookLoginButton
+                            className="social-login"
+                            text={t('login_with_facebook')}
+                            disabled={renderProps.disabled}
+                            onClick={renderProps.onClick}
+                          />
+                        )}
+                      />
+                    </div>
+                    <div
+                      className="col-6 p-0"
+                    >
+                      <GoogleLogin
+                        clientId="449870039809-vernaus5vu13rmqga2rf6t9lpofm9nuf.apps.googleusercontent.com"
+                        onSuccess={(res) => {
+                          this.responseGoogle(res);
+                        }}
+                        render={(renderProps) => (
+                          <GoogleLoginButton
+                            className="social-login"
+                            text={t('login_with_google')}
+                            disabled={renderProps.disabled}
+                            onClick={renderProps.onClick}
+                          />
+                        )}
+                      />
+                    </div>
+                  </div>
+                  <div className="row align-items-baseline">
+                    <div className="col-12 text-dark text-nowrap font-italic legal-subhead">
+                      <small>
+                        {t('signup-legal')}
+                      </small>
+                    </div>
+                  </div>
+                  <div className="row text-center mt-2">
+                    <div className="col-12">
+                      <button
+                        type="button"
+                        className="btn btn-outline-dark push-image-container"
+                        onClick={this.videoOnClick}
+                      >
+                        {t('watch-video')}
+                        {' '}
+                        <i className="far fa-play-circle" />
                       </button>
                     </div>
                   </div>
-                </form>
-                {error && (<p className="text-danger" style={{ margin: 0 }}>{t(error)}</p>)}
-                <div className="row">
-                  <div className="col-6 p-0">
-                    <FacebookLogin
-                      isMobile={false}
-                      appId="403863870540210"
-                      fields="email"
-                      callback={(res) => {
-                        this.responseFacebook(res);
-                      }}
-                      render={(renderProps) => (
-                        <FacebookLoginButton
-                          className="social-login"
-                          text={t('login_with_facebook')}
-                          disabled={renderProps.disabled}
-                          onClick={renderProps.onClick}
-                        />
-                      )}
-                    />
-                  </div>
-                  <div
-                    className="col-6 p-0"
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                  <OwlCarousel
+                    options={{
+                      items: 1,
+                      nav: true,
+                      rewind: true,
+                      autoplay: true,
+                      navText: [],
+                      dots: false,
+                    }}
+                    className="owl-carousel text-center"
+                    data-owl-nav="1"
+                    data-owl-loop="1"
                   >
-                    <GoogleLogin
-                      clientId="449870039809-vernaus5vu13rmqga2rf6t9lpofm9nuf.apps.googleusercontent.com"
-                      onSuccess={(res) => {
-                        this.responseGoogle(res);
-                      }}
-                      render={(renderProps) => (
-                        <GoogleLoginButton
-                          className="social-login"
-                          text={t('login_with_google')}
-                          disabled={renderProps.disabled}
-                          onClick={renderProps.onClick}
-                        />
-                      )}
+                    <img
+                      src={t('carousel-1')}
+                      className="d-inline-block mw-100 ts-width__auto"
+                      alt=""
                     />
-                  </div>
+                    <img
+                      src={t('carousel-2')}
+                      className="d-inline-block mw-100 ts-width__auto"
+                      alt=""
+                    />
+                    <img
+                      src={t('carousel-3')}
+                      className="d-inline-block mw-100 ts-width__auto"
+                      alt=""
+                    />
+                  </OwlCarousel>
                 </div>
-                <div className="row align-items-baseline">
-                  <div className="col-12 text-dark text-nowrap font-italic legal-subhead">
-                    <small>
-                      {t('signup-legal')}
-                    </small>
-                  </div>
-                </div>
-                <div className="row text-center mt-2">
-                  <div className="col-12">
-                    <button
-                      type="button"
-                      className="btn btn-outline-dark push-image-container"
-                      onClick={this.videoOnClick}
-                    >
-                      {t('watch-video')}
-                      {' '}
-                      <i className="far fa-play-circle" />
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-sm-12">
-                <OwlCarousel
-                  options={{
-                    items: 1,
-                    nav: true,
-                    rewind: true,
-                    autoplay: true,
-                    navText: [],
-                    dots: false,
-                  }}
-                  className="owl-carousel text-center"
-                  data-owl-nav="1"
-                  data-owl-loop="1"
-                >
-                  <img
-                    src={t('carousel-1')}
-                    className="d-inline-block mw-100 ts-width__auto"
-                    alt=""
-                  />
-                  <img
-                    src={t('carousel-2')}
-                    className="d-inline-block mw-100 ts-width__auto"
-                    alt=""
-                  />
-                  <img
-                    src={t('carousel-3')}
-                    className="d-inline-block mw-100 ts-width__auto"
-                    alt=""
-                  />
-                </OwlCarousel>
               </div>
             </div>
-          </div>
-        </header>
+          </header>
 
-        <div id="ts-dynamic-waves" className="ts-background" data-bg-color="rgba(255, 171, 3, 0.72)">
-          <svg
-            className="ts-svg"
-            width="100%"
-            height="100%"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
+          <div
+            id="ts-dynamic-waves"
+            className="ts-background"
+            data-bg-color="rgba(255, 171, 3, 0.72)"
           >
-            <defs />
-            <path
-              className="ts-dynamic-wave"
-              d=""
-              data-wave-color="#fff"
-              data-wave-height=".2"
-              data-wave-bones="6"
-              data-wave-speed="0.2"
-            />
-          </svg>
-        </div>
+            <svg
+              className="ts-svg"
+              width="100%"
+              height="100%"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs />
+              <path
+                className="ts-dynamic-wave"
+                d=""
+                data-wave-color="#fff"
+                data-wave-height=".2"
+                data-wave-bones="6"
+                data-wave-speed="0.2"
+              />
+            </svg>
+          </div>
 
-        <HomeFeatures t={t} />
-      </div>
+          <HomeFeatures t={t} />
+        </div>
+      </>
     );
   }
 }
