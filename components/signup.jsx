c5d9ba35fa1a500d6818662f0a3d2e3935d7d798
ahmedengu <ahmedengu@gmail.com>
2020-03-24 16:50:18 +0200

    client side email validate trim

--- components/signup.jsx
+++ components/signup.jsx
@@ -62,7 +62,7 @@ class Signup extends React.Component {
     const validateEmail = (email) => {
       const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(String(email)
-        .toLowerCase());
+        .toLowerCase().trim());
     };
 
     let { email } = this.state;
