fe38f0ea6161544ca12ec5dec3f76e99f1b1d793
ahmedengu <ahmedengu@gmail.com>
2020-03-25 08:44:20 +0200

    Structured Data

--- components/homePage.jsx
+++ components/homePage.jsx
@@ -11,6 +11,36 @@ const HomePage = ({
 }) => (
   <PageWrapper t={t} lang={lang}>
     {user && user.objectId ? <LoggedIn t={t} /> : <Signup t={t} />}
+
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: `
+        {
+          "@context": "http://schema.org",
+          "@type": "Organization",
+          "name": "${t('merquant')}",
+          "url": "https://merquant.com",
+          "logo": "${t('og-image')}",
+          "address": "Alexandria, Egypt",
+          "sameAs": [
+            "https://www.facebook.com/merquant",
+            "https://twitter.com/merquant",
+            "https://www.instagram.com/merquant",
+            "https://www.youtube.com/channel/UC5sXswGcRPseLizTDCltOJw",
+            "https://www.linkedin.com/company/merquant"
+          ],
+          "image": [
+            "https://merquant.com/${t('img-phone1')}",
+            "https://merquant.com/${t('img-whatis')}",
+            "https://merquant.com/${t('carousel-1')}",
+            "https://merquant.com/${t('carousel-2')}",
+            "https://merquant.com/${t('carousel-3')}"
+          ]
+        }
+    `,
+      }}
+    />
   </PageWrapper>
 );
 
